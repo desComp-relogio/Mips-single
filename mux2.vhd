a64bce234bc2540ec66b753ae990eb8dbd5f6ffd
vilela69 <frederico.vilelacurti@gmail.com>
2018-10-22 17:11:24 -0300

    um monte de coisa

--- mux2.vhd
+++ mux2.vhd
@@ -1,32 +1,28 @@
--- A library clause declares a name as a library.  It 
--- does not create the library; it simply forward declares 
--- it. 
 library IEEE;
--- STD_LOGIC and STD_LOGIC_VECTOR types, and relevant functions
-use ieee.std_logic_1164.all;
 
--- SIGNED and UNSIGNED types, and relevant functions
+use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity mux2 is
+	generic(
+		data_len: natural:= 32
+		);
 	port (
 		-- Input ports
-		A		: in  std_logic_vector(31 downto 0);
-		B		: in  std_logic_vector(31 downto 0);
+		A	: in  std_logic_vector(data_len-1 downto 0);
+		B	: in  std_logic_vector(data_len-1 downto 0);
 		SEL	: in  std_logic;
 		-- Output ports
-		Q		: out std_logic_vector(31 downto 0)
-	);
+		Q	: out std_logic_vector(data_len-1 downto 0));
 	
 end entity;
 architecture mux2Arch of mux2 is
-	-- Declarations (optional)
 begin
-	process(SEL, A, B)
+	process(all)
 	begin
-		 case SEL is
-			  when '0' => Q <= A;
-			  when '1' => Q <= B;
-		 end case;
+		case SEL is
+			when '0' => Q <= A;
+			when '1' => Q <= B;
+		end case;
 	end process;
 end architecture;
--- ulinha.vhd
+++ ulinha.vhd
@@ -9,39 +9,37 @@ entity ulinha is
 		-- Input ports
 		A	: in  std_logic_vector(31 downto 0);
 		B	: in  std_logic_vector(31 downto 0);
-		cin: in  std_logic;
+		CIN: in  std_logic;
 
 		-- Output ports
-		q	: out std_logic_vector(31 downto 0);
+		Q	: out std_logic_vector(31 downto 0);
 		Z	: out std_logic;
-		overflow : out std_logic
+		OVERFLOW : out std_logic
 	);
 	
 end ulinha;
 
-
-architecture arch_ulinha of ulinha is
-	signal cout:  std_logic_vector(31 downto 0);
-	signal inter: std_logic_vector(31 downto 0);
-	signal inter2: std_logic_vector(31 downto 0);
-	signal inter3: std_logic_vector(31 downto 0);
+architecture ulinhaArch of ulinha is
+	signal COUT:  std_logic_vector(31 downto 0);
+	signal INTER: std_logic_vector(31 downto 0);
+	signal INTER2: std_logic_vector(31 downto 0);
+	signal INTER3: std_logic_vector(31 downto 0);
 
 
 begin
-	inter <= a xor b; 
+	INTER <= A xor B; 
+	INTER2 <= A and B;
+	INTER3(0) <= INTER(0) and CIN;
+	INTER3(31 downto 1) <= INTER(31 downto 1) and COUT (30 downto 0);
+	COUT <= INTER2 xor INTER3;
 	
-	inter2 <= (a and b);
-	inter3(0) <= inter(0) and cin;
-	inter3(31 downto 1) <= inter(31 downto 1) and cout (30 downto 0);
-	cout <= inter2 xor inter3;
+	Q(0) <= INTER(0) xor CIN;
+	Q(31 downto 1) <= INTER(31 downto 1) xor COUT(30 downto 0);
 	
-	q(0) <= inter(0) xor cin;
-	q(31 downto 1) <= inter(31 downto 1) xor cout(30 downto 0);
-	
-	overflow <= cout(30) xor cout(31);
+	OVERFLOW <= COUT(30) xor COUT(31);
 
-	Z <= not(q(0) or q (1) or q(2) or q(3) or q(4) or q (5) or q (6) or q(7) or q(8) or q(9) or q(10)
-	      or q(11) or q(12) or q(13) or q(14) or q(15) or q(16) or q(17) or q(18) or q(19) or q(20) 
-			or q(21) or q(22) or q(23) or q(24) or q(25) or q(26) or q(27) or q(28) or q(29) or q(30) or q(31)) ;	
-	
-end arch_ulinha;
+	Z <= not(Q(0) or Q (1) or Q(2) or Q(3) or Q(4) or Q (5) or Q (6) or Q(7) or Q(8) or Q(9) or Q(10)
+	      or Q(11) or Q(12) or Q(13) or Q(14) or Q(15) or Q(16) or Q(17) or Q(18) or Q(19) or Q(20) 
+      or Q(21) or Q(22) or Q(23) or Q(24) or Q(25) or Q(26) or Q(27) or Q(28) or Q(29) or Q(30) or Q(31));	
+      
+end architecture;
